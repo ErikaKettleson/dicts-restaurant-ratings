9697e23c45ad5612de6f8995df059f671cff3368
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 18:03:43 -0800

    WIP user restaurant and rating input

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,29 @@
 # your code goes here
 
+# def get_user_restaurant_data():
+#     """prompts user for restaurant and rating and checks for int."""
+
+#     user_restaurant = raw_input(
+#         "We're rating restaurants! Give me a restaurant to rate!\n" +
+#         "> ")
+
+    
+#     while True:
+#         user_rating = raw_input("How would you rate your restaurant " +
+#                             "on a scale of 1-5?\n" + 
+#                             "> ")
+#         if int(user_rating) > 5 or int(user_rating) < 0:
+#             continue
+
+#         try int(user_rating):
+#             break
+#         except ValueError:
+#             continue
+
+
+
+
+
 
 def print_alpha_restaurant_ratings(filename):
     """prints restaurants and ratings in alphabetical order."""
@@ -9,9 +33,7 @@ def print_alpha_restaurant_ratings(filename):
     restaurant_ratings = {}
 
     for line in ratings_file:
-        restaurant_info = line.rstrip().split(":")
-        restaurant_name = restaurant_info[0]
-        restaurant_rating = restaurant_info[1]
+        restaurant_name, restaurant_rating = line.rstrip().split(":")
 
         restaurant_ratings[restaurant_name] = restaurant_rating
 
@@ -24,4 +46,5 @@ def print_alpha_restaurant_ratings(filename):
 
     ratings_file.close()
 
+get_user_restaurant_data()
 print_alpha_restaurant_ratings("scores.txt")
